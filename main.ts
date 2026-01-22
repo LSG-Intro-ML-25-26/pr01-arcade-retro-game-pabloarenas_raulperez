0755ec8536d8b08449042226419f2a5b9a5de6cc
Pablo Arenas <pablo.arenas@gracia.lasalle.cat>
2026-01-22 15:54:04 +0100

    Creacion de bloques iniciales de movimiento del personaje

--- main.ts
+++ main.ts
@@ -1,35 +1,25 @@
-controller.down.onEvent(ControllerButtonEvent.Pressed, function () {
-    animation.runImageAnimation(
-    nena,
-    assets.animation`nena-animation-down`,
-    500,
-    false
-    )
+controller.down.onEvent(ControllerButtonEvent.Pressed, function on_down_pressed() {
+    animation.runImageAnimation(nena, assets.animation`
+            nena-animation-down
+            `, 500, false)
 })
-controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
-    animation.runImageAnimation(
-    nena,
-    assets.animation`nena-animation-right`,
-    500,
-    false
-    )
+controller.right.onEvent(ControllerButtonEvent.Pressed, function on_right_pressed() {
+    animation.runImageAnimation(nena, assets.animation`
+            nena-animation-right
+            `, 500, false)
 })
-controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
-    animation.runImageAnimation(
-    nena,
-    assets.animation`nena-animation-left`,
-    500,
-    false
-    )
+controller.left.onEvent(ControllerButtonEvent.Pressed, function on_left_pressed() {
+    animation.runImageAnimation(nena, assets.animation`
+            nena-animation-left
+            `, 500, false)
 })
-controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
-    animation.runImageAnimation(
-    nena,
-    assets.animation`nena-animation-up`,
-    500,
-    false
-    )
+controller.up.onEvent(ControllerButtonEvent.Pressed, function on_up_pressed() {
+    animation.runImageAnimation(nena, assets.animation`
+            nena-animation-up
+            `, 500, false)
 })
-let nena: Sprite = null
-nena = sprites.create(assets.image`nena-front`, SpriteKind.Player)
+let nena : Sprite = null
+nena = sprites.create(assets.image`
+    nena-front
+    `, SpriteKind.Player)
 controller.moveSprite(nena)
